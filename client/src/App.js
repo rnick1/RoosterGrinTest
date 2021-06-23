ef58817cda032f39c9f67585cbfdad7e71ba002f
rnick1 <rnick0260@gmail.com>
2021-06-23 14:36:55 -0700

    Took code out of App and placed in Landing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
 import "./App.css";
+import Landing from "./pages/Landing/Landing.js";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+  return <Landing />;
 }
