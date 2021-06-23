74449c7ba0a507e0692a8e6b2b0cc8ed1af77d13
rnick1 <rnick0260@gmail.com>
2021-06-23 15:06:36 -0700

    Added unstyled content to the landing page.

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,19 +1,35 @@
 import React from "react";
 import "./Landing.css";
-import hero from "../images/hero.jpg";
-import logoWhite from "../images/logo-white.svg";
-import logoColor from "../images/logo-color.svg";
+// import hero from "../images/hero.jpg";
+import logoWhite from "../../images/logoWhite.svg";
+// import logoColor from "../images/logo-color.svg";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Landing() {
   return (
-    <div className="App">
-      <div className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div className="landing-background">
+      <Container>
+        <Row>
+          <Col>
+            <img src={logoWhite} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>WELCOME TO</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Rooster Grin</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <button>Button Button</button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
