f845486df346c02970e3d2b1780ffaea8a439506
rnick1 <rnick0260@gmail.com>
2021-06-24 10:28:16 -0700

    Setup FeaturesLeadin page folder.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Landing from "./pages/Landing/Landing.js";
 import About from "./pages/About/About.js";
-import FeatureLeadin from "./pages/FeatureLeadin.js";
+import FeatureLeadin from "./pages/FeaturesLeadin/FeaturesLeadin.js";
 import Feature from "./pages/Features.js";
 import TestimonialLeadin from "./pages/TestimonialLeadin.js";
 import Testimonial from "./pages/Testimonial.js";
