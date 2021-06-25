bb91c9894798f58625978fe7b47c30f4983518a7
rnick1 <rnick0260@gmail.com>
2021-06-25 13:16:35 -0700

    Improved responsiveness of the about page and added an arrow png.

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -5,7 +5,7 @@ import img1 from "../../images/img1.jpg";
 
 export default function About() {
   return (
-    <div>
+    <Container className="about-container" fluid={true}>
       <Row>
         <Col className="about-left">
           <h1 className="about-title">LOREM IPSUM</h1>
@@ -15,8 +15,7 @@ export default function About() {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+            pariatur."
           </p>
           <a className="about-link" href="#featuresleadin">
             `{">"}` Lorem Ipsum
@@ -26,6 +25,6 @@ export default function About() {
           <img className="about-img" src={img1} />
         </Col>
       </Row>
-    </div>
+    </Container>
   );
 }
