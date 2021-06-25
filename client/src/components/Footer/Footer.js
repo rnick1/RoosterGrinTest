4a43af35d61b1131133537bb828ccc470d76a1d5
rnick1 <rnick0260@gmail.com>
2021-06-24 19:54:47 -0700

    Split testimonial page into two col's and added more content. Did some work on the styling. Started work on the footer.

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>This is some content in sticky footer</p>
+    </div>
+  );
+}
--- client/src/pages/Testimonials/Testimonials.js
+++ client/src/pages/Testimonials/Testimonials.js
@@ -5,86 +5,52 @@ import Carousel from "react-bootstrap/Carousel";
 
 export default function Testimonials() {
   return (
-    <Container>
+    <Container className="testimonials-background">
       <Row>
-        <Carousel className="carousel">
-          <Carousel.Item>
-            <h3 className="carousel-name">John Doe 08/08/20</h3>
-            <p className="carousel-text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3 className="carousel-name">Flynn Rider 09/13/19</h3>
-            <p className="carousel-text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </Carousel.Item>
-          <Carousel.Item>
-            <h3 className="carousel-name">Jose Panera 04/05/21</h3>
-            <p className="carousel-text">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </Carousel.Item>
-        </Carousel>
-      </Row>
-    </Container>
-  );
-}
-
-{
-  /* <Col className="testimonials-left">
+        <Col>
           <h1 className="testimonials-title">LOREM IPSUM DOLOR SIT AMET</h1>
         </Col>
-        <Col className="testimonials-right">
-          <Carousel>
+        <Col>
+          <Carousel className="carousel">
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                // src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>John Doe 08/08/20</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
+              <h3 className="carousel-name">John Doe 08/08/20</h3>
+              <p className="carousel-text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
             </Carousel.Item>
             <Carousel.Item>
-              <Carousel.Caption>
-                <h3>Flynn Rider 04/03/21</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
+              <h3 className="carousel-name">Flynn Rider 09/13/19</h3>
+              <p className="carousel-text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
             </Carousel.Item>
             <Carousel.Item>
-              <Carousel.Caption>
-                <h3>Jose Panera 07/01/20</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
+              <h3 className="carousel-name">Jose Panera 04/05/21</h3>
+              <p className="carousel-text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
             </Carousel.Item>
           </Carousel>
         </Col>
-*/
+      </Row>
+    </Container>
+  );
 }
