585d16b0d529739809acb1d91a6c8be994d2357d
rnick1 <rnick0260@gmail.com>
2021-06-24 18:04:18 -0700

    Setup folder structure for both testimonial leadin and testimonial pages. Also setup basic structure for the testimonial leadin page.

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import Landing from "./pages/Landing/Landing.js";
 import About from "./pages/About/About.js";
 import FeaturesLeadin from "./pages/FeaturesLeadin/FeaturesLeadin.js";
 import Features from "./pages/Features/Features.js";
-import TestimonialsLeadin from "./pages/TestimonialsLeadin.js";
-import Testimonial from "./pages/Testimonial.js";
+import TestimonialsLeadin from "./pages/TestimonialsLeadin/TestimonialsLeadin.js";
+import Testimonials from "./pages/Testimonials/Testimonials.js";
 import { HashRouter, Route } from "react-router-dom";
 
 export default function App() {
@@ -16,8 +16,12 @@ export default function App() {
         <Route exact path="/about" component={About} />
         <Route exact path="/featuresleadin" component={FeaturesLeadin} />
         <Route exact path="/features" component={Features} />
-        <Route exact path="/testimonialleadin" component={TestimonialsLeadin} />
-        <Route exact path="/testimonial" component={Testimonial} />
+        <Route
+          exact
+          path="/testimonialsleadin"
+          component={TestimonialsLeadin}
+        />
+        <Route exact path="/testimonials" component={Testimonials} />
       </div>
     </HashRouter>
   );
---
+++ client/src/pages/TestimonialsLeadin/TestimonialsLeadin.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TestimonialsLeadin.css";
+// import { Container, Button } from "react-bootstrap";
+
+export default function TestimonialsLeadin() {
+  return (
+    <div className="testimonialsleadin-background">
+      {/* <Container>
+          <h1 className="featuresleadin-title">LOREM IPSUM</h1>
+          <Button className="featuresleadin-button" href="#features">
+            BUTTON BUTTON
+          </Button>
+        </Container> */}
+    </div>
+  );
+}
