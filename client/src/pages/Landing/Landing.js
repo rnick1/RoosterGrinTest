71e5d535be29ef22237e2d8798263398284cbeac
rnick1 <rnick0260@gmail.com>
2021-06-25 09:36:43 -0700

    Added fade-in effect to the landing page.

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -2,34 +2,37 @@ import React from "react";
 import "./Landing.css";
 import logoWhite from "../../images/logoWhite.svg";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import FadeIn from "react-fade-in";
 
 export default function Landing() {
   return (
-    <div className="landing-background">
-      <Container>
-        <Row>
-          <Col>
-            <img className="landing-logo" src={logoWhite} />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1 className="first-line-landing-text">WELCOME TO</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1 className="second-line-landing-text">Rooster Grin</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="landing-button-position">
-            <Button className="landing-button" href="#about">
-              BUTTON BUTTON
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <FadeIn transitionDuration={3000}>
+      <div className="landing-background">
+        <Container>
+          <Row>
+            <Col>
+              <img className="landing-logo" src={logoWhite} />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h1 className="first-line-landing-text">WELCOME TO</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h1 className="second-line-landing-text">Rooster Grin</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="landing-button-position">
+              <Button className="landing-button" href="#about">
+                BUTTON BUTTON
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </FadeIn>
   );
 }
