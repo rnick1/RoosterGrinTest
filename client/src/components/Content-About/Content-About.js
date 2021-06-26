e6d2e8587b23bbd77f101baffbc362a9ce15abdc
rnick1 <rnick0260@gmail.com>
2021-06-26 13:01:14 -0700

    Created component for about page content.

---
+++ client/src/components/Content-About/Content-About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Content-About.css";
+
+export default function ContentAbout() {
+  return (
+    <p className="about-text">
+      Rooster Grin Media is a web design company that specializes in crafting
+      custom websites at an affordable rate. Our photo- and video-centric
+      approach to creating websites is what sets us apart from other companies
+      in the industry. Sites built by Rooster Grin give companies a major leg up
+      in any competitive business landscape.
+    </p>
+  );
+}
--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -3,6 +3,7 @@ import "./About.css";
 import { Container, Row, Col } from "react-bootstrap";
 import img1 from "../../images/img1.jpg";
 import next from "../../images/next.png";
+import ContentAbout from "../../components/Content-About/Content-About";
 
 export default function About() {
   return (
@@ -10,13 +11,7 @@ export default function About() {
       <Row>
         <Col className="about-left">
           <h1 className="about-title">ABOUT US:</h1>
-          <p className="about-text">
-            Rooster Grin Media is a web design company that specializes in
-            crafting custom websites at an affordable rate. Our photo- and
-            video-centric approach to creating websites is what sets us apart
-            from other companies in the industry. Sites built by Rooster Grin
-            give companies a major leg up in any competitive business landscape.
-          </p>
+          <ContentAbout />
           <a className="about-link" href="#servicesleadin">
             Next
           </a>
