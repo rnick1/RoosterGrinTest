932ff7186bdb9dfbce58e23658fc16e4fc9b4270
rnick1 <rnick0260@gmail.com>
2021-06-26 13:11:54 -0700

    Added content component for the testimonials leadin page.

---
+++ client/src/components/Content-TestimonialsLeadin/Content-TestimonialsLeadin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Content-TestimonialsLeadin.css";
+
+export default function ContentTestimonialsLeadin() {
+  return (
+    <p className="testimonialsleadin-text">
+      We work hard for our customers. Running a business is never easy, so we
+      strive to ensure our products meet expectations and become an effective
+      force-multiplier for each company we are priviledged to work with.
+    </p>
+  );
+}
--- client/src/pages/TestimonialsLeadin/TestimonialsLeadin.js
+++ client/src/pages/TestimonialsLeadin/TestimonialsLeadin.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./TestimonialsLeadin.css";
 import { Container, Button } from "react-bootstrap";
+import ContentTestimonialsLeadin from "../../components/Content-TestimonialsLeadin/Content-TestimonialsLeadin";
 
 export default function TestimonialsLeadin() {
   return (
     <div className="testimonialsleadin-background">
       <Container>
         <h1 className="testimonialsleadin-title">Testimonials</h1>
-        <p className="testimonialsleadin-text">
-          We work hard for our customers. Running a business is never easy, so
-          we strive to ensure our products meet expectations and become an
-          effective force-multiplier for each company we are priviledged to work
-          with.
-        </p>
+        <ContentTestimonialsLeadin />
         <Button className="testimonialsleadin-button" href="#testimonials">
           TESTIMONIALS
         </Button>
